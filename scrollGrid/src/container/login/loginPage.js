7a32a8789796f5b2b2d6fa444cb3daea7e1695ac
Mkeshnitp4u <mukeshnitp4u@gmail.com>
2020-04-27 09:25:46 +0530

    scrollview add in logi page

--- scrollGrid/src/container/login/loginPage.js
+++ scrollGrid/src/container/login/loginPage.js
@@ -6,6 +6,7 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import COLOR from '../../assets/color/color';
 export default class loginPage extends Component {
@@ -17,56 +18,58 @@ export default class loginPage extends Component {
   render() {
     return (
       <SafeAreaView style={styles.safeContainer}>
-        <View style={styles.container}>
-          <View style={styles.welcomeContainer}>
-            <Text style={styles.welcomeText}> Login </Text>
-          </View>
-          <View style={styles.textInputContainer}>
-            <TextInput
-              style={styles.textInputStyle}
-              placeholder="Enter Email or Contact Number"
-              onChangeText={(text) => this.setState({email: text})}
-            />
-            {!this.state.email ? (
-              <>
-                <Text style={styles.errorStyle}>
-                  Please Enter Email/Contact
-                </Text>
-              </>
-            ) : null}
-            <TextInput />
+        <ScrollView>
+          <View style={styles.container}>
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}> Login </Text>
+            </View>
+            <View style={styles.textInputContainer}>
+              <TextInput
+                style={styles.textInputStyle}
+                placeholder="Enter Email or Contact Number"
+                onChangeText={(text) => this.setState({email: text})}
+              />
+              {!this.state.email ? (
+                <>
+                  <Text style={styles.errorStyle}>
+                    Please Enter Email/Contact
+                  </Text>
+                </>
+              ) : null}
+              <TextInput />
 
-            <TextInput
-              style={styles.textInputStyle}
-              placeholder="Enter Password"
-              secureTextEntry={true}
-              onChangeText={(text) => this.setState({password: text})}
-            />
-            {!this.state.password ? (
-              <>
-                <Text style={styles.errorStyle}>Please Enter Password</Text>
-              </>
-            ) : null}
+              <TextInput
+                style={styles.textInputStyle}
+                placeholder="Enter Password"
+                secureTextEntry={true}
+                onChangeText={(text) => this.setState({password: text})}
+              />
+              {!this.state.password ? (
+                <>
+                  <Text style={styles.errorStyle}>Please Enter Password</Text>
+                </>
+              ) : null}
 
-            <TextInput />
-          </View>
+              <TextInput />
+            </View>
 
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate('DashboardTab')}
-              disabled={!(this.state.email && this.state.password)}
-              style={[
-                styles.buttonStyle,
-                {
-                  backgroundColor: !(this.state.email && this.state.password)
-                    ? COLOR.disableButtonColorWithHalfOpacity
-                    : COLOR.enableButtonColor,
-                },
-              ]}>
-              <Text style={styles.submitText}> Submit </Text>
-            </TouchableOpacity>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                onPress={() => this.props.navigation.navigate('DashboardTab')}
+                disabled={!(this.state.email && this.state.password)}
+                style={[
+                  styles.buttonStyle,
+                  {
+                    backgroundColor: !(this.state.email && this.state.password)
+                      ? COLOR.disableButtonColorWithHalfOpacity
+                      : COLOR.enableButtonColor,
+                  },
+                ]}>
+                <Text style={styles.submitText}> Submit </Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
+        </ScrollView>
       </SafeAreaView>
     );
   }
